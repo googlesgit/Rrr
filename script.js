3de63aa4210f0c4a73290b63024adf2ad9fbcda4
googlesgit <miyapuramsairamreddy@gmail.com>
2026-04-28 02:34:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const btn = document.getElementById("revealBtn");
 const message = document.getElementById("hiddenMessage");
+const bgm = document.getElementById("bgm");
+
+// Play music after first interaction
+document.body.addEventListener("click", () => {
+  if (bgm.paused) {
+    bgm.play();
+  }
+}, { once: true });
 
 btn.addEventListener("click", () => {
   message.classList.add("show");
